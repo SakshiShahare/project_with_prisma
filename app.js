941f499654264615b036204cf0fe00b7e4afdadb
Sakshi <sakshi@Sakshis-MacBook-Pro.local>
2024-04-30 12:34:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+// the project revolves around the basics of prisma and how prisma makes the user happy by ease of work
+// we are goind to build a simple project that allows the user to upload many pictures 
+// one to many relationship 
